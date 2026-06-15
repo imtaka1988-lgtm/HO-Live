cf2e4c8d3888fdba08b2fe6cb59dfd4464a77df8
imtaka <imtaka1988@gmail.com>
2026-06-15 20:49:40 +0700

    cleanup: restrict cors default
    Only enable CORS middleware when CORS_ORIGIN is explicitly configured.

--- haiou-api/server.js
+++ haiou-api/server.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 });
 
 const corsOptions = buildCorsOptions();
-app.use(corsOptions ? cors(corsOptions) : cors());
+if (corsOptions) app.use(cors(corsOptions));
 app.use(express.json({ limit: "4mb" }));
 
 app.use((err, req, res, next) => {
